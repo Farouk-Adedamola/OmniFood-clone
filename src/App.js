6141a7c8e93c058e2a240a0e6f9e17c9c3e39f5e
Farouk adedamola <faroukadedamola1@gmail.com>
2022-10-19 00:43:24 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import classes from "./App.module.css";
-import Navbar from "./Components/Navbar";
+import Navbar from "./Components/Navbar/Navbar";
 import Featured from "./Components/belowHeader/Featured";
 import HowItworks from "./Components/HowItworks/HowItworks";
 import Meals from "./Components/Meals/Meals";
-import Testimonials from "./Components/Testimonials/Testimonials";
-import Pricing from "./Components/Pricing/Pricing";
-import Advantages from "./Components/Advantages/Advantages";
-import Card from "./Components/Card/Card";
+
 function App() {
   return (
     <>
-      <section className={classes.components}>
-        {/* <Navbar /> */}
-        <Featured className={classes.featured} />
+      <section className={classes.Navbar}>
+        <Navbar />;
+        <Featured />
         <HowItworks />
         <Meals />
-        <Testimonials />
-        <Pricing />
-        <Advantages />
-        <Card />
         <p>
-          ggggggggggggggggggggggg ggggjwwwwwwwwwwwwwwwwbq
-          qqqqqqqqqqqqqqqqqqqqqqqq qqqqqqqqqqveeeeeeeeeeeee
-          eeeeeeehhhhhhhhhhhhhhhhh hhhkwwwwwwwwwwwwwwwwjkkk kkkkkkkkkkkkkwbebbbb
+          shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+          kkkddjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          djjjjjjjjjjjjjjjjjjjjjjjjjjjjdjjjjjj
+          shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+          kkkddjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          djjjjjjjjjjjjjjjjjjjjjjjjjjjjdjjjjjj
+          shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+          kkkddjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          djjjjjjjjjjjjjjjjjjjjjjjjjjjjdjjjjjj
+          shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+          kkkddjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+          djjjjjjjjjjjjjjjjjjjjjjjjjjjjdjjjjjj
         </p>
       </section>
     </>
---
+++ src/Components/Navbar/navData.js
@@ -0,0 +1,36 @@
+import { hover } from "@testing-library/user-event/dist/hover";
+import React from "react";
+
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/about",
+    text: "how it works",
+  },
+  {
+    id: 3,
+    url: "/projects",
+    text: "meals",
+  },
+  {
+    id: 4,
+    url: "/contact",
+    text: "testimonial",
+  },
+  {
+    id: 5,
+    url: "/profile",
+    text: "pricing",
+  },
+  {
+    css: { background: "#CF711F", color: "#fff" },
+    id: 6,
+    url: "/profile",
+    text: "try for free",
+  },
+];
